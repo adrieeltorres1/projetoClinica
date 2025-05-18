0511372da6fc32073edbe1fb8d03a42992922be9
adrieeltorres1 <adrieltorreslds@gmail.com>
2025-05-17 23:12:03 -0300

    Atualizando ideias para dashboard

--- src/pages/admin/Dashboard.jsx
+++ src/pages/admin/Dashboard.jsx
@@ -7,3 +7,23 @@ const Dashboard = () => {
 }
 
 export default Dashboard;
+
+// Oque pode ir em Dashboard
+
+// Visão Geral de Atendimentos: Número de consultas agendadas, realizadas e canceladas.
+
+// Satisfação dos Pacientes: Indicadores baseados em pesquisas de satisfação ou feedbacks.
+
+// Ocupação da Clínica: Taxa de ocupação por médico, especialidade ou sala.
+
+// Indicadores Financeiros: Faturamento mensal, despesas fixas e variáveis, inadimplência.
+
+// Gestão de Estoque: Medicamentos e materiais disponíveis, previsões de reposição.
+
+// Tempo Médio de Espera: Para consultas, exames ou procedimentos.
+
+// Indicadores de Saúde Pública: Dados sobre tipos de atendimentos mais comuns, doenças registradas, sazonalidade.
+
+// Performance da Equipe: Produtividade dos profissionais, tempo médio de consulta, avaliações.
+
+// Gráfico de consultas agendadas. 
